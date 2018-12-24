db6dda1762e72d7c7caba1a290abef5326664b27
ASH Walls <ariswalls@gmail.com>
2018-12-24 13:00:57 -0700

    bandsintown is working

--- liri.js
+++ liri.js
@@ -3,8 +3,26 @@ require("dotenv").config();
 const keys = require('./keys');
 const [, , liriCommand, ...clArgs] = process.argv;
 clArg = clArgs.join(' ');
-const axios = require('axios')
+const axios = require('axios');
+var moment = require('moment');
+moment().format();
 const Spotify = require('node-spotify-api');
+//***********************************************************************//
+
+//BANDSINTOWN: **********************************************************//
+const searchBandsintown = (band) => {
+    return axios.get(`https://rest.bandsintown.com/artists/${band}/events?app_id=codingbootcamp`);
+}
+const formatBandsintown = (concerts) => {
+    const formattedConcerts = '\nUPCOMING CONCERTS:\n---------------------\n' + concerts.map(concert => {
+        return `Venue: ${concert.venue.name}
+        \rLocation: ${concert.venue.city}, ${concert.venue.region} ${concert.venue.country}
+        \rDate: ${moment(concert.datetime.split('T')[0], 'YYYY-MM-DD').format('MM/DD/YYYY')}`
+        
+    }).join('\n---------------------\n') + '\n';
+    
+    return formattedConcerts;
+}
 //***********************************************************************//
 
 //SPOTIFY: **************************************************************//
@@ -27,12 +45,7 @@ const formatSpotify = (song) => {
 }
 //***********************************************************************//
 
-//BANDSINTOWN: **********************************************************//
-const searchBandsintown = (band) => {
-    return axios.get(`https://rest.bandsintown.com/artists/${band}/events?app_id=codingbootcamp`);
-}
 
-//***********************************************************************//
 
 
 const lirify = (command) => {
@@ -40,7 +53,7 @@ const lirify = (command) => {
         case ('concert-this'): {
             searchBandsintown(clArg)
             .then((response) => {
-                console.log(response)
+                console.log( formatBandsintown(response.data) );
             })
             break;
         }
